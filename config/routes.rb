77ce0c771001acb4e674bd5ec995e1fa8b61d970
dev1980 <brham1980@gmail.com>
2021-05-14 18:03:41 +0545

    Create Views,Controllers for users and urls

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'urls/index'
+  get 'urls/new'
+  get 'users/new'
+  get 'users/edit'
+  get 'users/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
