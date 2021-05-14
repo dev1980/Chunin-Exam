e6ee3343e9b4fbac273c6f7a4a4ec0a8d52285f0
dev1980 <brham1980@gmail.com>
2021-05-14 18:05:32 +0545

    update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'urls/index'
-  get 'urls/new'
-  get 'users/new'
-  get 'users/edit'
-  get 'users/show'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+resources :urls, only: [:index, :new, :create, :show]
+resources :users, only: [:index, :new, :create, :show]
+
 end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
